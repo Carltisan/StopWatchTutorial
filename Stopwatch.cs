e695488b9f98756031fccbec32c9df2e45956da9
Carltisan <33442250+Carltisan@users.noreply.github.com>
2019-02-08 14:57:17 +1300

    Add project files.

---
+++ Stopwatch.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopWatchTutorial
+{
+    public class StopWatch
+    {
+
+        public DateTime? StartTime { get; set; }
+
+        public TimeSpan? EndTime
+        {
+            get
+            {
+                
+                var timeSpan = DateTime.Now - StartTime;
+
+                return timeSpan;
+            }
+        }
+
+        public void Start()
+        {
+            if (!StartTime.HasValue)
+            {
+                StartTime = DateTime.Now;
+                Console.WriteLine("Stop Watch Started");
+            }
+            else
+            {
+                throw new InvalidOperationException("Watch already started");
+            }
+
+
+           
+        }
+
+        public void Stop()
+        {
+            if (StartTime.HasValue)
+            {
+                Console.WriteLine(EndTime);
+                StartTime = null;
+            }
+            else
+            {
+                throw new InvalidOperationException("Please start Watch first");
+            }
+            
+        }
+       
+    }
+}
---
+++ program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StopWatchTutorial
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            var stopWatch = new StopWatch();
+
+            string value = "";
+
+            Console.WriteLine("Type start, stop or end");
+
+            while (value != "end"){
+
+            value = Console.ReadLine();
+
+            if (value == "start")
+            {
+                stopWatch.Start();
+            }
+            else if (value == "stop")
+            {
+                stopWatch.Stop();
+            }
+
+            }
+
+
+
+        }
+    }
+    
+} 
